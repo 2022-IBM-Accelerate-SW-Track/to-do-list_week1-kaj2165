8b84997b82b600374ceff17926cf0d9578648371
kaj2165 <kaj2165@columbia.edu>
2022-06-13 11:44:11 -0400

    working website without testing code

--- src/component/AddTodo.js
+++ src/component/AddTodo.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-
+import {Button, TextField} from "@mui/material";
 class AddTodo extends Component {
     // A local react state of the this component with a content property set to nothing.
     constructor() {
       super();
       this.state = {
-        content: "",
+        content: ""
       };
     }
     // The handleChange function updates the react state with the new input value provided from the user.
@@ -36,9 +36,22 @@ class AddTodo extends Component {
         // 2. The return should also include a button with the handleSubmit function from above that is passed into
         // an OnClick event.
         // 3. The value of the text field also should reflect the local state of this component.
-        <div>
-          
-        </div>
+      <div>
+         <TextField
+        label="Add New Item"
+        variant="outlined"
+        onChange={this.handleChange}
+        value={this.state.content}
+        />
+        <Button
+        style={{ marginLeft: "10px" }}
+        onClick={this.handleSubmit}
+        variant="contained"
+        color="primary"
+        >
+        Add
+        </Button>
+      </div>
       );
     }
   }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import about_pic from "../assets/about_pic.jpeg"; 
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <p><div>
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_pic"
+              src={about_pic}
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Kelly Jones</div>
+            <div className="brief_description">
+              <p>
+                I am a rising junior at Columbia majoring in Computer Science and minoring in Statistics. I am from Alexandria, Virginia and will be doing an internship in LA this summer. I enjoy reading and going for walks.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div> </p> 
       </div>
     )
   }
